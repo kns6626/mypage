05e6be62a5ceb9961f08e46d8f39f015a52a403b
kns6626 <68229282+kns6626@users.noreply.github.com>
2020-08-07 18:13:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function() {
+  var headerHeight = $('header').outerHeight(),
+    startPos = 0;
+  $(window).on('load scroll', function() {
+    var scrollPos = $(this).scrollTop();
+    if (scrollPos > startPos && scrollPos > headerHeight) {
+      $('header').fadeOut();
+    } else {
+      $('header').fadeIn();
+    }
+    startPos = scrollPos;
+  });
+});
